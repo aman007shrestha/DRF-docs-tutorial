1274ec2074bc771adda0d235bf51842523532cba
aman <KAN007BCT@student.kec.edu.np>
2020-12-11 19:33:10 +0545

    2nd part

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
 	#path('', views.index, name="index"),
 	path('snippets/', views.snippet_list),
 	path('snippets/<int:pk>', views.snippet_detail),
 ]
+urlpatterns = format_suffix_patterns(urlpatterns)
--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Snippet
 from .serializers import SnippetSerializer
 
@@ -12,47 +16,47 @@ from .serializers import SnippetSerializer
 from pygments.lexers import get_all_lexers
 from pygments.styles import get_all_styles
 
-@csrf_exempt
-def snippet_list(request):
+#@csrf_exempt
+@api_view(['GET', 'POST'])
+def snippet_list(request, format=None):
 	"""
 	List all code snippets, or create a new snippet.
 	"""
 	if request.method == 'GET':
 		snippets = Snippet.objects.all()
 		serializer = SnippetSerializer(snippets, many=True)
-		return JsonResponse(serializer.data, safe=False)
+		return Response(serializer.data)
 
 	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = SnippetSerializer(data=data)
+		serializer = SnippetSerializer(data=request.data)
 		if serializer.is_valid():
 			serializer.save()
-			return JsonResponse(serializer.data, status=201)
-		return JsonResponse(serializer.errors, status=400)
+			return Response(serializer.data, status=status.HTTP_201_CREATED)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
+#@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
+def snippet_detail(request, pk, format=None):
 	"""
 	Retrieve, update or delete a code snippet.
 	"""
 	try:
 		snippet = Snippet.objects.get(pk=pk)
 	except Snippet.DoesNotExist:
-		return HttpResponse(status=404)
+		return Response(status=status.HTTP_404_NOT_FOUND)
 	if request.method == 'GET':
 		serializer = SnippetSerializer(snippet)
-		return JsonResponse(serializer.data)
-	elif request.method == 'POST':
-		data = JSONParser().parse(request)
-		serializer = SnippetSerializer(snippet, data=data)
+		return Response(serializer.data)
+	elif request.method == 'PUT':
+		serializer = SnippetSerializer(snippet, data=request.data)
 		if serializer.is_valid():
 			serializer.save()
-			return JsonResponse(serializer.data)
-		return JsonResponse(serializer.errors, status=400)
+			return Response(serializer.data)
+		return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 	elif request.method == 'DELETE':
 		snippet.delete()
-		return HttpResponse(status=204)
+		return Response(status=status.HTTP_204_NO_CONTENT)
 
 def index(request):
 	LEXERS = [item for item in get_all_lexers() if item[1]]
